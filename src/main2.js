dcb21a838e18ac8ba6cd49b6e9b680a007333d63
Emnolope <44859532+Emnolope@users.noreply.github.com>
2023-04-28 02:54:42 +0000

    Trying to switch module/package/serving/transpilation/import/export system

---
+++ src/main2.js
@@ -0,0 +1,73 @@
+// Import A-Frame and troika-three-text
+const AFRAME = require('aframe');
+const { Text } = require('troika-three-text');
+
+// Create a basic A-Frame scene
+const scene = document.createElement('a-scene');
+
+// Configure the text properties for both texts
+const textProps = [
+  {
+    value: 'Hello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\nHello, World!\n',
+    position: '0 1 -2'
+  },
+  {
+    value: 'Space Goodbye!\nSpace Goodbye!\nSpace Goodbye!\nSpace Goodbye!\nSpace Goodbye!\nSpace Goodbye!\n',
+    position: '0 1 -2',
+    rotation: '0 10 -20'
+  }
+];
+
+// Function to combine geometries
+function combineGeometries(geo1, geo2) {
+  const geometry1 = geo1.clone();
+  const geometry2 = geo2.clone();
+
+  geometry2.translate(0, -geometry1.boundingBox.getSize(new THREE.Vector3()).y, 0);
+  geometry1.merge(geometry2);
+
+  return geometry1;
+}
+
+// Create Troika Text instances for both texts
+const texts = textProps.map((props) => {
+  const text = new Text();
+  text.text = props.value;
+  text.fontSize = 0.2;
+  text.color = 0xff0000;
+  return text;
+});
+
+// Function to handle the completion of rendering for both texts
+function onTextsRendered(geometries) {
+  // Combine the geometries
+  const combinedGeometry = combineGeometries(geometries[0], geometries[1]);
+
+  // Create a new mesh with the combined geometry
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const combinedMesh = new THREE.Mesh(combinedGeometry, material);
+
+  // Create an A-Frame entity to hold the combined mesh
+  const combinedEntity = document.createElement('a-entity');
+  combinedEntity.setObject3D('mesh', combinedMesh);
+  combinedEntity.setAttribute('position', '0 1 -2');
+  scene.appendChild(combinedEntity);
+}
+
+// Render the texts and wait for their completion
+let textsRendered = 0;
+const textGeometries = [];
+
+texts.forEach((text, index) => {
+  text.sync(() => {
+    textGeometries[index] = text.geometry;
+    textsRendered++;
+
+    if (textsRendered === texts.length) {
+      onTextsRendered(textGeometries);
+    }
+  });
+});
+
+// Add the scene to the DOM
+document.body.appendChild(scene);
---
+++ src/main3.js
@@ -0,0 +1,62 @@
+const THREE = require('three');
+//const { BufferGeometryUtils } = require('three/examples/jsm/utils/BufferGeometryUtils.js');
+
+// Create a WebGLRenderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Create a scene
+const scene = new THREE.Scene();
+
+// Create a camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create geometries for a sphere, a box, and a torus
+const sphereGeometry = new THREE.SphereGeometry(1, 16, 16);
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const torusGeometry = new THREE.TorusGeometry(0.5, 0.2, 16, 100);
+
+// Position the geometries
+sphereGeometry.translate(-2, 0, 0);
+boxGeometry.translate(0, 0, 0);
+torusGeometry.translate(2, 0, 0);
+
+/*// Merge the geometries
+const mergedGeometry = BufferGeometryUtils.mergeBufferGeometries([
+  sphereGeometry,
+  boxGeometry,
+  torusGeometry
+]);*/
+
+// Create a material
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+
+const sphereMesh = new THREE.Mesh(sphereGeometry, material);
+const boxMesh = new THREE.Mesh(boxGeometry, material);
+const torusMesh = new THREE.Mesh(torusGeometry, material);
+scene.add(sphereMesh);
+scene.add(boxMesh);
+scene.add(torusMesh);
+
+/*// Create a mesh with the merged geometry and material
+const mergedMesh = new THREE.Mesh(mergedGeometry, material);
+
+// Add the mesh to the scene
+scene.add(mergedMesh);*/
+
+// Create the animation loop
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  // Rotate the mesh
+  boxMesh.rotation.x += 0.01;
+  boxMesh.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+};
+
+// Start the animation loop
+animate();
